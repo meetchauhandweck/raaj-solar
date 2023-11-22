77c562e5d72ddbae4ecf0d269ec2bc6d019dddb0
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2023-11-22 18:22:26 +0530

    22-11-23 3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route exact path="/raaj-solar" element={<Home />} />
+        <Route index path="/raaj-solar" element={<Home />} />
         <Route path="/raaj-solar/company" element={<Company />} />
         <Route path="/raaj-solar/products" element={<Products />} />
         <Route path="/raaj-solar/experience" element={<Experience />} />
